85ede1f698097916a12a917c30247f1013523ede
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-05-25 21:44:53 +0100

    making my SignUp Page

--- src/App.jsx
+++ src/App.jsx
@@ -2,27 +2,22 @@
 import './App.css';
 import PreLoader from './components/Preloader';
 
-import Login from './components/Login';
+import SignUp from './components/SignUp';
 // import { Routes, Route } from "react-router-dom"
 
 
 function App() {
  
 
-
-
-
   return (
     <>
       
 <PreLoader />
 
-<Login />
-{/* <div>
-  <h2>sooooooooooooo</h2>
-  </div> */}
+<SignUp />
+
         {/* <Routes>
-        <Route path="login" element={ <Login/> } />
+        <Route path="SignUp" element={ <SignUp/> } />
         <Route index element={ <main /> } />
       </Routes> */}
 
