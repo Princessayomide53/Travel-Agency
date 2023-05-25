98fc2b904369d2a1b9e73bb99e98b697763344e9
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-05-25 23:57:31 +0100

    removed an icon, that keeps throwing error

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Signup from "../assets/Signup.png";
-import {MdAlternateEmail} from "react-icons/md"
  import {GoOctoface} from "react-icons/go"
  import {SlLock} from "react-icons/sl"
  import {ImMobile2} from "react-icons/im"
@@ -18,7 +17,7 @@ function SignUp() {
       <form>
       <div className='sm:space-y-16 pt-12 space-y-14 items-center flex flex-col '>
         <div className='flex space-x-7'>
-        <MdAlternateEmail size={30}/>
+        <p className='font-bold text-3xl'>@</p>
         <input type="text" placeholder="Email" className='border-b-2 border-black sm:w-96 w-80 rounded-md h-8 pl-3'/>
         </div>
         <div className='flex space-x-7'><GoOctoface size={30}/>
