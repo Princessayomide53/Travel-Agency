fd76948c6f8566d449effbcb27a1118a5442c080
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-05-25 23:37:46 +0100

    finished up with SignUp page

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -11,12 +11,12 @@ function SignUp() {
 <div className=' flex sm:flex-row sm:justify-evenly flex-col'>
   <div className='flex justify-center'>
   <img src={Signup} alt="illustration"
-   className='w-[400px] h-[400px] sm:w-[550px] sm:h-[620px]'/>
+   className='w-[400px] h-[400px] sm:w-[580px] sm:h-[650px]'/>
   </div>
     <div>
       <h1 className='text-black text-5xl font-serif text-center pt-5 sm:pt-10 font-bold'>Sign Up</h1>
       <form>
-      <div className='sm:space-y-20 pt-12 space-y-14 items-center flex flex-col '>
+      <div className='sm:space-y-16 pt-12 space-y-14 items-center flex flex-col '>
         <div className='flex space-x-7'>
         <MdAlternateEmail size={30}/>
         <input type="text" placeholder="Email" className='border-b-2 border-black sm:w-96 w-80 rounded-md h-8 pl-3'/>
@@ -30,7 +30,10 @@ function SignUp() {
           <ImMobile2 size={30}/>
           <input type="text" placeholder="Phone No" className='border-b-2 border-black sm:w-96 w-80 pl-3 rounded-md h-8 mb-3 sm:mb-0' />
         </div>
-        <p className='text-gray-500 text-sm font-mono'>By signing up, you agreed to our <span className='text-blue-500'>Terms & Conditions and Privacy Policy</span></p>
+      <p className='text-gray-500 sm:text-sm text-xs font-sans'>By signing up, you agreed to our <span className='font-semibold text-blue-500'>Terms & Conditions and Privacy Policy</span></p>
+
+      <div className='flex-row flex space-x-10 pb-3 sm:mb-0'> <button className='bg-blue-600 font-semibold sm:text-base text-sm rounded-2xl px-10 py-2 text-white '>Continue </button> 
+      <p className='text-gray-500 sm:text-sm text-xs font-sans font-semibold mt-[10px]'>Joined us before? <span className='text-blue-600 font-semibold'>Login</span></p></div>
       </div>
       </form>
       </div>
