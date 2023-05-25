9d18b33b30142f00b768927186b6db32569bd6a1
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-05-26 00:50:34 +0100

    Still adjusting

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -7,26 +7,26 @@ import handshake from "../assets/handshake.png";
 function SignUp() {
   return (
 <section>
-<div className=' flex sm:flex-row sm:justify-evenly flex-col'>
+<div className='overflow-hidden flex sm:flex-row sm:justify-evenly flex-col'>
   <div className='flex justify-center'>
   <img src={handshake} alt="illustration"
-   className='w-[100px] h-[100px] sm:w-[580px] sm:h-[650px]'/>
+   className='w-[50px] h-[50px] sm:w-[580px] sm:h-[650px]'/>
   </div>
-    <div>
+    <div className='w-[90%] sm:w-0 mx-auto sm:m-0'>
       <h1 className='text-black text-5xl font-serif text-center pt-5 sm:pt-10 font-bold'>Sign Up</h1>
       <form>
       <div className='sm:space-y-16 pt-12 space-y-14 items-center flex flex-col pl-5 pr-5 sm:pl-0 sm:pr-0'>
         <div className='flex space-x-7'>
-        <p className='font-bold text-3xl'>@</p>
+        <p className='font-bold sm:text-3xl text-base'>@</p>
         <input type="text" placeholder="Email" className='border-b-2 border-black sm:w-96 w-56 rounded-md h-8 pl-3'/>
         </div>
-        <div className='flex space-x-7'><GoOctoface className='sm:text-3xl text-xl'/>
+        <div className='flex space-x-7'><GoOctoface className='sm:text-3xl text-base'/>
         <input type="text" placeholder="Full Name" className='border-b-2 border-black sm:w-96 w-56 rounded-md h-8 pl-3'/></div>
-        <div className='flex space-x-7'><SlLock className='sm:text-3xl text-xl'/> 
+        <div className='flex space-x-7'><SlLock className='sm:text-3xl text-base'/> 
         <input type="text" placeholder="Password" className='border-b-2 border-black sm:w-96 w-56 rounded-md h-8 pl-3'/></div>
         
         <div className='flex space-x-7 '>
-          <ImMobile2 className='sm:text-3xl text-xl'/>
+          <ImMobile2 className='sm:text-3xl text-base'/>
           <input type="text" placeholder="Phone No" className='border-b-2 border-black sm:w-96 w-56 pl-3 rounded-md h-8 mb-3 sm:mb-0' />
         </div>
       <p className='text-gray-500 sm:text-sm text-xs font-sans'>By signing up, you agreed to our <span className='font-semibold text-blue-500'>Terms & Conditions and Privacy Policy</span></p>
