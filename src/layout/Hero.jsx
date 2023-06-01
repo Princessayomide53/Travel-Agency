720e0f82a670f721a19fe473459c31b90453021e
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-06-01 17:43:07 +0100

    adjusted the image

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -13,11 +13,11 @@ function Hero() {
           <img
             src={decore}
             alt=""
-            className="sm:-mt-20 sm:w-[500px] sm: w-[220px] h-[550px] -mt-[57px]"
+            className="sm:-mt-20 sm:w-[500px] sm: w-[220px] h-[350px] sm:h-[550px] -mt-[57px]"
           />
         </div>
         <div className="z-50 sm:mt-10 sm:pl-10 flex sm:flex-row flex-col sm:justify-between">
-          <div className="sm:mt-10 pt-0 sm:pl-10 md:pl-5 pl-10 lg:pl-12">
+          <div className="sm:mt-10 pt-52 md:pt-0 sm:pl-10 md:pl-5 pl-10 lg:pl-12">
           <p className="text-[#DF6951] -mt-[440px]  font-Poppins font-bold sm:pl-0 md:text-xl text-sm lg:text-2xl">
             Best Destinations Around the world
           </p>
@@ -26,8 +26,8 @@ function Hero() {
             <br /> and live a new <br /> and full life
           </h1>
           </div>
-          <div className="z-20 pr-20 lg:pr-[145px]">
-          <img src={image} alt="" className="sm:w-[400px] sm:h-[400px] sm:-mt-[450px] md:w-[300px] md:h-[300px] -mt-[190px]"/>
+          <div className="z-20 pr-20 lg:pr-[160px]">
+          <img src={image} alt="" className="sm:w-[400px] sm:h-[400px] sm:-mt-[450px] md:w-[300px] md:h-[300px] -mt-[190px] w-[300px] h-[300px]"/>
           </div>
           
         </div>
