dbb9115fdee59f6792351584acb59f373ff1aaa5
Princess Ayomide Ogunnaike <payomide53@gmail.com>
2023-06-01 20:17:27 +0100

    added a paragraph

--- src/layout/Hero.jsx
+++ src/layout/Hero.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Nav from "./Nav";
 import decore from "../../src/assets/decore.png";
-import image from "../../src/assets/image.png";
+import Image from "../../src/assets/Image.png";
 // import { Cross as Hamburger } from 'hamburger-react'
 
 function Hero() {
@@ -18,18 +18,26 @@ function Hero() {
         </div>
         <div className="z-50 sm:mt-10 sm:pl-10 flex sm:flex-row flex-col sm:justify-between">
           <div className="sm:mt-10 pt-52 md:pt-0 sm:pl-10 md:pl-5 pl-10 lg:pl-12">
-          <p className="text-[#DF6951] -mt-[440px]  font-Poppins font-bold sm:pl-0 md:text-xl text-sm lg:text-2xl">
-            Best Destinations Around the world
-          </p>
-          <h1 className="font-bold lg:text-6xl md:text-5xl sm:pt-3 pt-4 font-Volkhov sm:pl-0 text-[37px]">
-            Travel, enjoy
-            <br /> and live a new <br /> and full life
-          </h1>
+            <p className="text-[#DF6951] -mt-[440px]  font-Poppins font-bold sm:pl-0 md:text-xl text-sm lg:text-2xl">
+              Best Destinations Around the world
+            </p>
+            <h1 className="font-bold lg:text-6xl md:text-5xl sm:pt-3 pt-4 font-Volkhov sm:pl-0 text-[37px]">
+              Travel, enjoy
+              <br /> and live a new <br /> and full life
+            </h1>
+            <p className="sm:text-sm sm:pt-5 font-normal">
+              Built Wicket longer admire do barton vanity itself do in it.
+              <br /> Preferred to sportsmen it engrossed listening. Park gate
+              <br /> sell they west hard for the.
+            </p>
           </div>
-          <div className="z-20 pr-20 lg:pr-[160px]">
-          <img src={image} alt="" className="sm:w-[400px] sm:h-[400px] sm:-mt-[450px] md:w-[300px] md:h-[300px] -mt-[190px] w-[300px] h-[300px]"/>
+          <div className="z-20 pr-20 lg:pr-[120px]">
+            <img
+              src={Image}
+              alt=""
+              className="lg:w-[450px] lg:h-[450px] sm:-mt-[450px] md:w-[300px] md:h-[300px] -mt-[190px] w-[300px] h-[300px]"
+            />
           </div>
-          
         </div>
       </section>
     </>
--- src/layout/Nav.jsx
+++ src/layout/Nav.jsx
@@ -2,41 +2,50 @@ import Hamburger from "hamburger-react";
 import React, { useState } from "react";
 
 function Nav() {
-  const [open, setOpen] = useState(false)
+  const [open, setOpen] = useState(false);
   return (
     <section>
       <nav>
         <div className="hidden sm:block">
-      <div className="flex sm:flex-row flex-col sm:pt-5 z-50 sm:justify-between"> 
-        <h1 className="sm:text-5xl text-[#212832] font-semibold md:pl-12 sm:pl-28 ">Jadoo <span className="fill-[#F1A501] bg-inherit"></span></h1>
-      <div className="z-10 sm:pr-28 md:pr-12">
-          <ul className="flex sm:space-x-20 z-50 sm:pt-5 font-normal sm:text-[14px] text-[#212832]">
-          <li>Destinations</li>
-          <li>Hotels</li>
-          <li>Flights</li>
-          <li>Bookings</li>
-        </ul></div>
+          <div className="flex sm:flex-row flex-col sm:pt-5 z-50 sm:justify-between">
+            <h1 className="sm:text-5xl text-[#212832] font-semibold md:pl-12 sm:pl-28">
+              Jadoo
+            </h1>
+            <div className="z-10 sm:pr-28 md:pr-12">
+              <ul className="flex sm:space-x-20 z-50 sm:pt-4 font-normal sm:text-[14px] text-[#212832]">
+                <li>Destinations</li>
+                <li>Hotels</li>
+                <li>Flights</li>
+                <li>Bookings</li>
+              </ul>
+            </div>
+          </div>
         </div>
-      </div>
-      
-      <div className="flex flex-col pt-2  z-50 sm:hidden"> 
-      <div className="flex justify-between pr-5">
-        <h1 className="text-4xl text-[#212832] font-semibold pl-5 h-[1px] z-10">Jadoo<span className=""></span></h1>
-        <Hamburger  size={25} className="mr-5" direction="left" onToggle={() => setOpen(!open)}/>
-        </div>
-        {open &&
-        <div className="z-10 bg-white h-screen">
-          <ul className="flex flex-col space-y-14 z-50 pl-5 pt-7  font-normal text-base text-[#212832]">
-          <li>Destinations</li>
-          <li>Hotels</li>
-          <li>Flights</li>
-          <li>Bookings</li>
-        </ul></div>
-} 
 
-      </div>
+        <div className="flex flex-col pt-2  z-50 sm:hidden">
+          <div className="flex justify-between pr-5">
+            <h1 className="text-4xl text-[#212832] font-semibold pl-5 h-[1px] z-10">
+              Jadoo<span className=""></span>
+            </h1>
+            <Hamburger
+              size={25}
+              className="mr-5"
+              direction="left"
+              onToggle={() => setOpen(!open)}
+            />
+          </div>
+          {open && (
+            <div className="z-10 bg-white h-screen">
+              <ul className="flex flex-col space-y-14 z-50 pl-5 pt-7  font-normal text-base text-[#212832]">
+                <li>Destinations</li>
+                <li>Hotels</li>
+                <li>Flights</li>
+                <li>Bookings</li>
+              </ul>
+            </div>
+          )}
+        </div>
       </nav>
-      
     </section>
   );
 }
